b7b220afe83c9feb4b753b40fc2912b23873252f
Ulthran <ctbushman@gmail.com>
2025-03-12 12:22:42 -0400

    Add db functions

---
+++ marc_db/db.py
@@ -0,0 +1,26 @@
+from sqlalchemy import create_engine
+from marc_db.models import Base
+
+def create_database(database_url):
+    """
+    Create the database using the provided database URL.
+    
+    Parameters:
+    database_url (str): The database URL.
+    """
+    engine = create_engine(database_url)
+    Base.metadata.create_all(engine)
+
+def get_connection(database_url):
+    """
+    Get a connection to the database using the provided database URL.
+    
+    Parameters:
+    database_url (str): The database URL.
+    
+    Returns:
+    connection: The connection to the database.
+    """
+    engine = create_engine(database_url)
+    connection = engine.connect()
+    return connection
