56512811a00fb406062fc3ef109912e7ced1d7c6
Charlie <ctbushman@gmail.com>
2025-07-15 09:22:56 -0400

    Use assembly_id as primary key for Antimicrobial

--- marc_db/models.py
+++ marc_db/models.py
@@ -1,4 +1,12 @@
-from sqlalchemy import Column, Integer, Text, Date, ForeignKey, UniqueConstraint
+from sqlalchemy import (
+    Column,
+    Integer,
+    Text,
+    Date,
+    Float,
+    ForeignKey,
+    UniqueConstraint,
+)
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
@@ -32,3 +40,53 @@ class Aliquot(Base):
     isolate_id = Column(Text, ForeignKey("isolates.sample_id"), nullable=False)
     tube_barcode = Column(Text)
     box_name = Column(Text)
+
+
+class Assembly(Base):
+    __tablename__ = "assemblies"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    isolate_id = Column(Text, ForeignKey("isolates.sample_id"), nullable=False)
+    run_number = Column(Text)
+    sunbeam_version = Column(Text)
+    sbx_sga_version = Column(Text)
+    config_file = Column(Text)
+    assembly_fasta_path = Column(Text)
+
+
+class AssemblyQC(Base):
+    __tablename__ = "assembly_qc"
+
+    assembly_id = Column(Integer, ForeignKey("assemblies.id"), primary_key=True)
+    contig_count = Column(Integer)
+    genome_size = Column(Integer)
+    n50 = Column(Integer)
+    gc_content = Column(Float)
+    cds = Column(Integer)
+    completeness = Column(Float)
+    contamination = Column(Float)
+    min_contig_coverage = Column(Float)
+    avg_contig_coverage = Column(Float)
+    max_contig_coverage = Column(Float)
+
+
+class TaxonomicAssignment(Base):
+    __tablename__ = "taxonomic_assignments"
+
+    assembly_id = Column(Integer, ForeignKey("assemblies.id"), primary_key=True)
+    taxonomic_classification = Column(Text)
+    st = Column(Text)
+    st_schema = Column(Text)
+    allele_assignment = Column(Text)
+
+
+class Antimicrobial(Base):
+    __tablename__ = "antimicrobials"
+
+    assembly_id = Column(Integer, ForeignKey("assemblies.id"), primary_key=True)
+    contig_id = Column(Text)
+    gene_symbol = Column(Text)
+    gene_name = Column(Text)
+    accession = Column(Text)
+    element_type = Column(Text)
+    resistance_product = Column(Text)
