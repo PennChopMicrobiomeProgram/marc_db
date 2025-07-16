4f6213a9d37c9c517d9a3653cb56a6ff23ffa10f
Charlie <ctbushman@gmail.com>
2025-07-16 10:02:41 -0400

    fix: ensure db helpers work with python 3.9

--- marc_db/db.py
+++ marc_db/db.py
@@ -14,18 +14,23 @@ def get_marc_db_url() -> str:
     return os.environ["MARC_DB_URL"]
 
 
-def create_database(database_url: str = get_marc_db_url()):
+from typing import Optional
+
+
+def create_database(database_url: Optional[str] = None):
     """
     Create the database tables that don't exist using the provided database URL.
 
     Parameters:
     database_url (str): The database URL.
     """
+    if database_url is None:
+        database_url = get_marc_db_url()
     engine = create_engine(database_url)
     Base.metadata.create_all(engine, checkfirst=True)
 
 
-def get_connection(database_url: str = get_marc_db_url()) -> Connection:
+def get_connection(database_url: Optional[str] = None) -> Connection:
     """
     Get a connection to the database using the provided database URL.
 
@@ -35,12 +40,14 @@ def get_connection(database_url: str = get_marc_db_url()) -> Connection:
     Returns:
     connection: The connection to the database.
     """
+    if database_url is None:
+        database_url = get_marc_db_url()
     engine = create_engine(database_url)
     connection = engine.connect()
     return connection
 
 
-def get_session(database_url: str = get_marc_db_url()) -> Session:
+def get_session(database_url: Optional[str] = None) -> Session:
     """
     Get a session to the database using the provided database URL.
 
@@ -52,6 +59,8 @@ def get_session(database_url: str = get_marc_db_url()) -> Session:
     """
     from sqlalchemy.orm import sessionmaker
 
+    if database_url is None:
+        database_url = get_marc_db_url()
     engine = create_engine(database_url)
     Session = sessionmaker(bind=engine)
     session = Session()
